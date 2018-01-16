0f41e0a51d9c5bc99dc12dbeafca0c533015c608
Genius <35426080+Aashu1997@users.noreply.github.com>
2018-01-16 08:47:26 +0530

    Add files via upload

---
+++ assets/circle-counter/circle counter/examples.js
@@ -0,0 +1,63 @@
+
+$('.ci.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(100 * progress) + '<i>%</i>');
+});
+
+
+$('.c2.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(80 * progress) + '<i>%</i>');
+});
+
+
+
+$('.c3.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(50 * progress) + '<i>%</i>');
+});
+
+
+$('.c4.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(50 * progress) + '<i>%</i>');
+});
+
+
+
+$('.c5.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(50 * progress) + '<i>%</i>');
+});
+
+
+
+$('.c6.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(65 * progress) + '<i>%</i>');
+});
+
+
+
+$('.c7.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(65 * progress) + '<i>%</i>');
+});
+
+
+
+
+
+$('.c8.circle').circleProgress({
+    value: 0.6
+}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(parseInt(30 * progress) + '<i>%</i>');
+});
+
